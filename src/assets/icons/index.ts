c9e41973595a533aedbed6600ea5267d4a89144c
Imam Robani <imam.robani@sicepat.com>
2022-08-03 21:52:17 +0700

    feat: setup react native svg

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,19 @@
+import IcHomeOff from './ic-home-off.svg';
+import IcHomeOn from './ic-home-on.svg';
+import IcProfileOff from './ic-profile-off.svg';
+import IcProfileOn from './ic-profile-on.svg';
+import IcChevron from './ic-chevron.svg';
+import IcArrowLeft from './ic-arrow-left.svg';
+import IcPlusPurple from './ic-plus-purple.svg';
+import IcMinPurple from './ic-minus-purple.svg';
+
+export {
+  IcHomeOff,
+  IcHomeOn,
+  IcProfileOff,
+  IcProfileOn,
+  IcChevron,
+  IcArrowLeft,
+  IcPlusPurple,
+  IcMinPurple
+};
